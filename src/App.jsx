e5d033fc75ea74dcfeb94b9aaa412d6b64abcecf
Utkarsh Anand <utkarshanand133@gmail.com>
2025-08-06 23:09:44 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // src/App.jsx
 import { useState } from "react";
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Typography } from "antd";
+import { CheckCircleOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 import CalendarView from "./components/CalendarView";
 import TaskList from "./components/TaskList";
 import TaskChart from "./components/TaskChart";
 import TaskForm from "./components/TaskForm";
 
-const { Content } = Layout;
+const { Header, Content } = Layout;
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(
@@ -28,20 +29,47 @@ function App() {
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Content style={{ padding: "24px" }}>
-        <Row gutter={[24, 24]}>
+      {/* Header */}
+      <Header
+        style={{
+          background: "#001529",
+          padding: "0 24px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <CheckCircleOutlined
+          style={{ fontSize: 24, color: "#52c41a", marginRight: 12 }}
+        />
+        <Typography.Title level={3} style={{ color: "#fff", margin: 0 }}>
+          Task Manager
+        </Typography.Title>
+      </Header>
+
+      {/* Main Content */}
+      <Content style={{ padding: "32px" }}>
+        <Row gutter={[32, 32]}>
+          {/* Calendar + Task List */}
           <Col xs={24} md={12}>
             <CalendarView
               onDateSelect={handleDateSelect}
               onOpenModal={handleOpenModal}
             />
-            <TaskList selectedDate={selectedDate} onEditTask={handleEditTask} />
+            <div style={{ marginTop: "24px" }}>
+              <TaskList
+                selectedDate={selectedDate}
+                onEditTask={handleEditTask}
+              />
+            </div>
           </Col>
+
+          {/* Chart */}
           <Col xs={24} md={12}>
             <TaskChart />
           </Col>
         </Row>
 
+        {/* Modal */}
         <TaskForm
           open={isModalOpen}
           onCancel={() => setIsModalOpen(false)}
